f1f63bf49f20431ddcc522d38a2b0fb43a63d5b5
papat16 <timilehinsamuel505@gmail.com>
2025-01-13 15:35:13 +0100

    repair

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
+    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['@heroicons/react', '@headlessui/react', 'react-hot-toast'],
         },
       },
     },
