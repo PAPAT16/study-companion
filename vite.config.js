81c8ef3718f5820ee079b764d3bac0191b0aaf3a
papat16 <timilehinsamuel505@gmail.com>
2025-01-13 12:01:24 +0100

    vercel commit

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['@heroicons/react', '@headlessui/react']
-        }
-      }
-    }
+        },
+      },
+    },
   },
-  server: {
-    port: 3000
-  }
 })
